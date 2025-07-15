1a70292db5c3a9e90e7fb49a8005217121e0cf7f
yug <yugdesai517@gmail.com>
2025-07-15 21:43:11 +0530

    routes added

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,10 +1,24 @@
+'use client'
+import './index.css'
+import { Routes, Route } from 'react-router-dom';
+import GameRoomPage from './components/GameRoomPage';
+import RoomPage from './components/RoomPage';
+import LandingPage from './components/LandingPage';
+
+
+
+
+
+
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/room" element={<RoomPage />} />
+      <Route path="/room/:roomId" element={<GameRoomPage />} />
+    </Routes>
+  );
 }
 
 export default App;
